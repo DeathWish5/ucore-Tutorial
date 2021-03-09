addc902243d28da1a5d188801f951f3eced1bcbd
deathwish5 <zyr16@mails.tsinghua.edu.cn>
2021-03-09 19:50:01 +0800

    fix kernel.ld

--- kernel/main.c
+++ kernel/main.c
@@ -1,11 +1,11 @@
 #include "defs.h"
 
-extern char sbss[];
-extern char ebss[];
+extern char s_bss[];
+extern char e_bss[];
 
 void clean_bss() {
     char* p;
-    for(p = sbss; p < ebss; ++p)
+    for(p = s_bss; p < e_bss; ++p)
         *p = 0;
 }
 
--- kernel/types.h
+++ kernel/types.h
@@ -4,7 +4,7 @@ typedef unsigned char  uchar;
 typedef unsigned char uint8;
 typedef unsigned short uint16;
 typedef unsigned int  uint32;
-typedef unsigned long uint64;
+typedef unsigned long long uint64;
 typedef uint64 pte_t;
 typedef uint64 pde_t;
 typedef uint64 *pagetable_t;// 512 PTEs
